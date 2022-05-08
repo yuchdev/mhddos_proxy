e55a9c0e7022d1ad441330d0dd2555d96106420c
0xFFiber <0xffiber@gmail..com>
2022-05-08 16:15:13 +0200

    Even more aggressive

--- src/core.py
+++ src/core.py
@@ -33,9 +33,9 @@ PROXIES_URLS = (
 IT_ARMY_CONFIG_URL = 'https://gist.githubusercontent.com/ddosukraine2022/f739250dba308a7a2215617b17114be9/raw/mhdos_targets_tcp_v2.txt'
 VERSION_URL = 'https://raw.githubusercontent.com/porthole-ascend-cinnamon/mhddos_proxy/main/version.txt'
 
-LOW_RPC = 1000
-THREADS_PER_CORE = 2500
-MAX_DEFAULT_THREADS = 7500
+LOW_RPC = 1_000
+THREADS_PER_CORE = 2_500
+MAX_DEFAULT_THREADS = 10_000
 CONFIG_FETCH_RETRIES = 3
 CONFIG_FETCH_TIMEOUT = 10
 REFRESH_OVERTIME = 2  # roughly 5 more seconds
