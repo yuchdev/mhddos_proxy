e2d7d3d9b4333e6f1805c06769415a7aa1fb793a
OleksandrBlack <aleksandr.chornuy@gmail.com>
2022-06-26 22:27:35 +0300

    add_spanish_lng
    --lang ES

--- src/i18n.py
+++ src/i18n.py
@@ -1,127 +1,167 @@
 TRANSLATIONS = {
     'No working proxies found - stopping the attack': {
-        'ua': 'Не знайдено робочих проксі - зупиняємо атаку'
+        'ua': 'Не знайдено робочих проксі - зупиняємо атаку',
+        'es': 'No se han encontrado proxies que funcionen - parando el ataque'
     },
     'Selected': {
-        'ua': 'Обрано'
+        'ua': 'Обрано',
+        'es': 'Seleccionado'
     },
     'targets for the attack': {
-        'ua': 'цілей для атаки'
+        'ua': 'цілей для атаки',
+        'es': 'Objetivos para el ataque'
     },
     'Target': {
-        'ua': 'Ціль'
+        'ua': 'Ціль',
+        'es': 'Objetivo'
     },
     'Port': {
-        'ua': 'Порт'
+        'ua': 'Порт',
+        'es': 'Puerto'
     },
     'Method': {
-        'ua': 'Метод'
+        'ua': 'Метод',
+        'es': 'Método'
     },
     'Packets': {
-        'ua': 'Пакети'
+        'ua': 'Пакети',
+        'es': 'Paquetes'
     },
     'Traffic': {
-        'ua': 'Трафік'
+        'ua': 'Трафік',
+        'es': 'Tráfico'
     },
     'Total': {
-        'ua': 'Усього'
+        'ua': 'Усього',
+        'es': 'Total'
     },
     'Loaded config for': {
-        'ua': 'Завантажено конфіг на'
+        'ua': 'Завантажено конфіг на',
+        'es': 'Configuración cargada para'
     },
     'Targets loading failed': {
-        'ua': 'Завантаження цілей завершилося помилкою:'
+        'ua': 'Завантаження цілей завершилося помилкою:',
+        'es': 'La carga de objetivos ha fallado'
     },
     'No targets specified for the attack': {
-        'ua': 'Не вказано жодної цілі для атаки'
+        'ua': 'Не вказано жодної цілі для атаки',
+        'es': 'No hay objetivos especificados para el ataque'
     },
     'Launching the attack...': {
-        'ua': 'Запускаємо атаку...'
+        'ua': 'Запускаємо атаку...',
+        'es': 'Lanzando el ataque...'
     },
     'Empty config loaded - the previous one will be used': {
-        'ua': 'Завантажено порожній конфіг - буде використано попередній'
+        'ua': 'Завантажено порожній конфіг - буде використано попередній',
+        'es': 'Configuración vacía cargada - se utilizará la anterior'
     },
     'Failed to (re)load targets config:': {
-        'ua': 'Не вдалося (пере)завантажити конфіг цілей:'
+        'ua': 'Не вдалося (пере)завантажити конфіг цілей:',
+        'es': 'Error al (re)cargar la configuración de los objetivos:'
     },
     'Failed to reload proxy list - the previous one will be used': {
-        'ua': 'Не вдалося перезавантажити список проксі - буде використано попередній'
+        'ua': 'Не вдалося перезавантажити список проксі - буде використано попередній',
+        'es': 'Error al recargar la lista de proxies - se utilizará la anterior'
     },
     'A new version is available, update is recommended': {
-        'ua': 'Доступна нова версія, рекомендуємо оновити'
+        'ua': 'Доступна нова версія, рекомендуємо оновити',
+        'es': 'Una nueva versión está disponible. Se recomienda actualizar'
     },
     'The number of threads has been reduced to': {
-        'ua': 'Кількість потоків зменшено до'
+        'ua': 'Кількість потоків зменшено до',
+        'es': 'El número de hilos se ha reducido a'
     },
     'due to the limitations of your system': {
-        'ua': 'через обмеження вашої системи'
+        'ua': 'через обмеження вашої системи',
+        'es': 'due to the limitations of your system'
     },
     'Shutting down...': {
-        'ua': 'Завершуємо роботу...'
+        'ua': 'Завершуємо роботу...',
+        'es': 'Apagando...'
     },
     'The number of copies is automatically reduced to': {
-        'ua': 'Кількість копій автоматично зменшена до'
+        'ua': 'Кількість копій автоматично зменшена до',
+        'es': 'El número de copias se ha reducido automáticamente a'
     },
     'Threads': {
-        'ua': 'Потоків'
+        'ua': 'Потоків',
+        'es': 'Hilos'
     },
     'Using only proxies': {
-        'ua': 'Атака тільки через проксі'
+        'ua': 'Атака тільки через проксі',
+        'es': 'Usando solo proxies'
     },
     'Using both proxies and your IP/VPN': {
-        'ua': 'Атака через проксі та ваш IP/VPN'
+        'ua': 'Атака через проксі та ваш IP/VPN',
+        'es': 'Usando ambos, proxies y su IP/VPN'
     },
     'Using only your IP/VPN (no proxies)': {
-        'ua': 'Атака тільки через ваш IP/VPN (без проксі)'
+        'ua': 'Атака тільки через ваш IP/VPN (без проксі)',
+        'es': 'Usando solo su IP/VPN (no proxies)'
     },
     'Delay in execution of operations detected': {
-        'ua': 'Зафіксована затримка у виконанні операцій'
+        'ua': 'Зафіксована затримка у виконанні операцій',
+        'es': 'Detectado un retraso en la ejecución de operaciones'
     },
     'the attack continues, but we recommend reducing the workload': {
-        'ua': 'атака продовжується, але рекомендуємо зменшити значення навантаження'
+        'ua': 'атака продовжується, але рекомендуємо зменшити значення навантаження',
+        'es': 'el ataque continua, pero te recomendamos que reduzcas el volumen de trabajo'
     },
     'Workload (number of threads)': {
-        'ua': 'Навантаження (кількість потоків)'
+        'ua': 'Навантаження (кількість потоків)',
+        'es': 'Volumen de trabajo (número de hilos)'
     },
     'use flag `-t XXXX`, default is': {
-        'ua': 'параметр `-t XXXX`, за замовчуванням -'
+        'ua': 'параметр `-t XXXX`, за замовчуванням -',
+        'es': 'usa la bandera `-t XXXX`. Por defecto es'
     },
     'Complete documentation': {
-        'ua': 'Повна документація'
+        'ua': 'Повна документація',
+        'es': 'Documentación completa'
     },
     'Consider adding your IP/VPN to the attack - use flag `--vpn`': {
-        'ua': 'Щоб використовувати ваш IP/VPN на додачу до проксі: параметр `--vpn`'
+        'ua': 'Щоб використовувати ваш IP/VPN на додачу до проксі: параметр `--vpn`',
+        'es': 'Considera añadir tu IP/VPN al ataque - usa la bandera `--vpn`'
     },
     'Instead of high `-t` value consider using': {
-        'ua': 'Замість високого значення `-t` краще використати'
+        'ua': 'Замість високого значення `-t` краще використати',
+        'es': 'En vez de un valor `-t` alto, considera usar'
     },
     'The `--debug` option is deprecated to avoid negative impact on performance': {
-        'ua': 'Параметр `--debug` не підтримується через негативний вплив на продуктивність'
+        'ua': 'Параметр `--debug` не підтримується через негативний вплив на продуктивність',
+        'es': 'la opción `--debug` está obsoleta para evitar un impacto negativo en el rendimiento'
     },
     'Try running with automatic updates: ': {
-        'ua': 'Радимо запускати скрипт з автоматичним оновленням: '
+        'ua': 'Радимо запускати скрипт з автоматичним оновленням: ',
+        'es': 'Intenta ejecutar con actualizaciones automáticas'
     },
     '`uvloop` activated successfully': {
-        'ua': '`uvloop` успішно активовано'
+        'ua': '`uvloop` успішно активовано',
+        'es': '`uvloop` activado con éxito'
     },
     '(increased network efficiency)': {
-        'ua': '(підвищенна ефективність роботи з мережею)'
+        'ua': '(підвищенна ефективність роботи з мережею)',
+        'es': '(eficiencia de la red aumentada)'
     },
     'targets': {
-        'ua': 'цілей'
+        'ua': 'цілей',
+        'es': 'objetivos'
     },
     "is not available and won't be attacked": {
-        'ua': 'не доступна і не буде атакована'
+        'ua': 'не доступна і не буде атакована',
+        'es': 'no está disponible y no será atacado'
     },
     'Capacity': {
-        'ua': 'Потужність'
+        'ua': 'Потужність',
+        'es': 'Capacidad'
     },
     'Connections': {
-        'ua': "З'єднань"
+        'ua': "З'єднань",
+        'es': 'Conexiones'
     },
 }
 
-LANGUAGES = ['ua', 'en']
+LANGUAGES = ['ua', 'en', 'es']
 DEFAULT_LANGUAGE = LANGUAGES[0]
 
 
